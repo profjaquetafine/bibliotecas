a87034d354b2b11530f18f4877f4df77350fcdec
profjaquetafine <88409934+profjaquetafine@users.noreply.github.com>
2022-07-19 13:09:18 -0300

    Add files via upload

---
+++ Pedro Jogo/sketch.js
@@ -0,0 +1,31 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload(){
+ 
+  
+}
+ 
+ 
+function setup() {
+  createCanvas(800,800);
+
+  engine = Engine.create();
+  world = engine.world;
+
+ 
+ 
+
+ 
+}
+ 
+ 
+function draw() {
+  background("blue");
+  
+}
+ 
+
+
